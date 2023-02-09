5f8e791d9193ea7818f9c290650c17ecf3d70c2b
Dean Pearce <dapearce@live.unc.edu>
2023-02-09 15:02:58 -0500

    Split out parameters

--- mutation.m
+++ mutation.m
@@ -1,8 +1,5 @@
 % set variables
-prob_mutation = 1e-8;
-cultures = 30;
-generations = 20;
-initial_inoculum = 100;
+params;
 
 mut=zeros(cultures,1);
 for i=1:cultures
---
+++ params.m
@@ -0,0 +1,4 @@
+prob_mutation = 1e-8;
+cultures = 30;
+generations = 20;
+initial_inoculum = 100;
